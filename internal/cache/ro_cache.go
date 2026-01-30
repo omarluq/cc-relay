243f193550394ccc34c77c0580a43eaf57e9d5aa
omarluq <omar.luqman@hey.com>
2026-01-30 17:50:37 -0600

    chore: fix unnecessary var declartion

--- internal/cache/ro_cache.go
+++ internal/cache/ro_cache.go
@@ -188,7 +188,7 @@ func getCachedTyped[T any](ctx context.Context, c *ROCache, key string) (result 
 		return result, false, err
 	}
 
-	if err := json.Unmarshal(data, &result); err != nil {
+	if json.Unmarshal(data, &result) != nil {
 		return result, false, ErrCacheCorrupt
 	}
 	return result, true, nil
